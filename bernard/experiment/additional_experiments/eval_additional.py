323d7edd14b1925a97d5d1c0c1ead9908d200482
Tim Oliver Krause <tikrause@mail.uni-mannheim.de>
2023-06-13 23:02:26 +0200

    #5 adjusted scripts and results for clear overview

---
+++ bernard/experiment/additional_experiments/eval_additional.py
@@ -0,0 +1,7 @@
+import variance
+import sem
+import quantile_99_sem
+import quantile_99_stdev
+import buffer_less_all_logs
+import buffer_based_size1000
+import no_filtering
---
+++ bernard/experiment/eval_compl_config.py
@@ -0,0 +1,6 @@
+# This script runs all the scripts needed for the evaluation step 2 shown in the thesis (Choice of Configuration)
+
+import eval_buffer_less
+import eval_stdev
+import eval_quantile_95
+import eval_buffer_based
